06940ec2bebe5f0756c045698faf704810a75fa4
Kelsen <kelsenhazelwood@icloud.com>
2016-12-20 14:15:05 -0500

    Node class created

---
+++ Node.cpp
@@ -0,0 +1,55 @@
+//Node.cpp
+#include <iostream>
+#include <string>
+
+class Node {
+  protected:
+    int nodeNum;
+    Node left;
+    Node right;
+
+  public:
+    Node () {
+      nodeNum = 0;
+      left = NULL;
+      right = NULL;
+    } //end constructor
+
+    Node (int nodeNum) {
+      this.nodeNum = nodeNum;
+      left = NULL;
+      right = NULL
+    } //end constructor
+
+    int getNodeNum () {
+      return nodeNum;
+    } //end getNodeNum
+
+    Node getLeftNode () {
+      return left;
+    } //end getLeftNode
+
+    Node getRightNode () {
+      return right;
+    } //end getRightNode
+
+    void setLeftNode (Node left) {
+      this.left = left;
+    } //end setLeftNode
+
+    void setRightNode (Node right) {
+      this.right = right;
+    } //end setRightNode
+
+    int getLeftValue () {
+      Node leftNode = this.getLeftNode ();
+      int leftValue = leftNode.getNodeNum ();
+      return leftValue;
+    } //end getLeftValue 
+
+    int getRightValue () {
+      Node rightNode = this.getRightNode ();
+      int rightValue = rightNode.getNodeNum ();
+      return rightValue;
+    } //end getRightValue
+}; //end class
